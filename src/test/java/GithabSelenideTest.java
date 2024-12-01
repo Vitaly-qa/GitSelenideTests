9b842e96b90c50758c7786a5d72e19cc7080cd1e
Vitaly Kuzmin <vitalyqa34@gmail.com>
2024-12-02 00:23:51 +0300

    Initial commit

---
+++ src/test/java/GithabSelenideTest.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class GithabSelenideTest {
+    @Test
+void SelenideTest() {
+
+    open("https://github.com/selenide/selenide");
+    $("#wiki-tab").click ();
+    $("#wiki-pages-filter").setValue("SoftAssertions");
+    $("#wiki-pages-box").$(byText("SoftAssertions")).click();
+    $("#wiki-content").shouldHave(text(
+            "@ExtendWith({SoftAssertsExtension.class})\n" +
+                    "class Tests {\n" +
+                    "@Test\n" +
+                    "void test() {\n" +
+                    "Configuration.assertionMode = SOFT;\n" +
+                    "open(\"page.html\");\n" +
+                    "\n" +
+                    "$(\"#first\").should(visible).click();\n" +
+                    "$(\"#second\").should(visible).click();\n" +
+                    "}\n" +
+                    "}"));
+
+
+
+}
+}
+
+
